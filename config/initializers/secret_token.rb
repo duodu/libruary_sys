7729d7d51ebb1e2153840863d29e805686eb6d54
duoduo <zerolin_0428@163.com>
2013-05-20 21:39:53 +0800

    libruary_sys

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LibruarySys::Application.config.secret_token = 'dfee070dba4ed4dacc30c104785f617d55c4012c563060a272767fd79020cb576125f32884a1f470d7ed86167423aac6b51644637da769e0edb526b5cc86ee5c'
