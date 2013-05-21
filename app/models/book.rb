7e10883ca27b86dbd2c4b9cccc841a0597ba9193
duoduo <zerolin_0428@163.com>
2013-05-21 22:23:51 +0800

    model update

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,6 @@ class Book < ActiveRecord::Base
   attr_accessible :bookinfo, :bookname, :category_id, :stat_id, :storage, :uptime
   belongs_to :category
   belongs_to :stat
+  has_many :borrows
   has_many :users, :through => :borrows
 end
